b028a7f30f9e822ac3e84e91563167b36387fda3
Sambhav Jain <sambhav@alumni.stanford.edu>
2025-12-06 17:00:08 +0000

    fix

--- build_tools/docker/exec_docker_ci.sh
+++ build_tools/docker/exec_docker_ci.sh
@@ -20,5 +20,5 @@ docker run --rm \
            -v "${PWD}":/workspace \
            ${DOCKER_RUN_DEVICE_OPTS} \
            --security-opt seccomp=unconfined \
-           ghcr.io/sjain-stanford/compiler-dev-ubuntu-24.04:sambhav-rocm_bump@sha256:5e82e1e11a6e1717fd905dd871631b95bb741234205ddaa086aed55359dc75b4 \
+           ghcr.io/sjain-stanford/compiler-dev-ubuntu-24.04:sambhav-rocm_bump@sha256:3ea981f3a89948f9508af62d38657650da71047ab1d674a2d922294d05a0d693 \
            "$@"
