ca04920c5fd033ec5a36d97112e8483dcaac6caa
Ian Wood <ianwood2024@u.northwestern.edu>
2025-12-10 14:03:28 -0800

    Bump IREE in docker to 3.10.0rc20251210 (#41)
    Signed-off-by: Ian Wood <ianwood@u.northwestern.edu>

--- build_tools/docker/exec_docker_ci.sh
+++ build_tools/docker/exec_docker_ci.sh
@@ -20,5 +20,5 @@ docker run --rm \
            -v "${PWD}":/workspace \
            ${DOCKER_RUN_DEVICE_OPTS} \
            --security-opt seccomp=unconfined \
-           ghcr.io/sjain-stanford/compiler-dev-ubuntu-24.04:main@sha256:5d12763cfc48eaddfd35bd4e87033c45ce5d326096846dd77b02357d0af3652a \
+           ghcr.io/sjain-stanford/compiler-dev-ubuntu-24.04:main@sha256:971f215965336f207dc8ad750b3b4edc42825cba97118bf63fa2dddcf29f4158 \
            "$@"
