1c2160edbfff3eab4a8fc3841070225ea161c062
Rob Suderman <rob.suderman@gmail.com>
2026-01-23 12:13:42 -0800

    pre-commit run
    Signed-off-by: Rob Suderman <rob.suderman@gmail.com>

--- include/fusilli/backend/compile_session.h
+++ include/fusilli/backend/compile_session.h
@@ -580,7 +580,7 @@ CompileSession::build(const Handle &handle, const CacheFile &input,
   FUSILLI_LOG_LABEL_ENDL("INFO: Building compile session");
 
   // Create compiler context.
-  auto* context = FUSILLI_TRY(CompileContext::create());
+  auto *context = FUSILLI_TRY(CompileContext::create());
 
   // Create session with backend-specific flags.
   auto session = FUSILLI_TRY(context->createSession(handle));
